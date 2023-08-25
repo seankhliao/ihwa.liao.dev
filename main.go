fb5eb50dd4c091da716f05d4ac7671b6cf8b9e50
Sean Liao <sean+git@liao.dev>
2023-08-25 22:43:11 +0100

    refresh

--- main.go
+++ main.go
@@ -4,16 +4,13 @@ import (
 	"bytes"
 	"context"
 	_ "embed"
-	"flag"
 	"fmt"
 	"log/slog"
 	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
 	"time"
 
-	"go.seankhliao.com/svcrunner/v2/basehttp"
+	"go.seankhliao.com/svcrunner/v3/framework"
+	"go.seankhliao.com/svcrunner/v3/observability"
 	"go.seankhliao.com/webstyle"
 )
 
@@ -21,52 +18,33 @@ import (
 var rawIndex []byte
 
 func main() {
-	fset := flag.NewFlagSet("ihwa.liao.dev", flag.ExitOnError)
-	conf := &basehttp.Config{}
-	conf.SetFlags(fset)
-	fset.Parse(os.Args[1:])
+	framework.Run(framework.Config{
+		Start: func(ctx context.Context, o *observability.O, m *http.ServeMux) (func(), error) {
+			t0 := time.Now()
+			index, err := webstyle.NewRenderer(webstyle.TemplateCompact).RenderBytes(rawIndex, webstyle.Data{})
+			if err != nil {
+				return nil, fmt.Errorf("render index: %w", err)
+			}
+			m.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+				ctx, span := o.T.Start(r.Context(), "handle request")
+				defer span.End()
 
-	ctx := context.Background()
-	ctx, cancel := signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM)
-	defer cancel()
+				o.L.LogAttrs(ctx, slog.LevelDebug, "handle request", slog.Group("http.request",
+					slog.String("proto", r.Proto),
+					slog.String("method", r.Method),
+					slog.String("host", r.Host),
+					slog.String("path", r.URL.Path),
+					slog.String("remote", r.RemoteAddr),
+					slog.Any("headers", r.Header),
+				))
 
-	svr := basehttp.New(ctx, conf)
-
-	err := run(ctx, svr)
-	if err != nil {
-		svr.O.L.LogAttrs(ctx, slog.LevelError, "server exit", slog.String("err", err.Error()))
-		os.Exit(1)
-	}
-}
-
-func run(ctx context.Context, svr *basehttp.Server) error {
-	t0 := time.Now()
-	index, err := webstyle.NewRenderer(webstyle.TemplateCompact).RenderBytes(rawIndex, webstyle.Data{})
-	if err != nil {
-		return fmt.Errorf("render index: %w", err)
-	}
-	svr.Mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("ok"))
+				if r.Method != http.MethodGet {
+					http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+					return
+				}
+				http.ServeContent(w, r, "index.html", t0, bytes.NewReader(index))
+			})
+			return nil, nil
+		},
 	})
-	svr.Mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		ctx, span := svr.O.T.Start(r.Context(), "handle request")
-		defer span.End()
-
-		svr.O.L.LogAttrs(ctx, slog.LevelDebug, "handle request", slog.Group("http.request",
-			slog.String("proto", r.Proto),
-			slog.String("method", r.Method),
-			slog.String("host", r.Host),
-			slog.String("path", r.URL.Path),
-			slog.String("remote", r.RemoteAddr),
-			slog.Any("headers", r.Header),
-		))
-
-		if r.Method != http.MethodGet {
-			http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
-			return
-		}
-		http.ServeContent(w, r, "index.html", t0, bytes.NewReader(index))
-	})
-
-	return svr.Run(ctx)
 }
